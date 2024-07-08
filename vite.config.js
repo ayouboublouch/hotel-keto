821ee30b698f2864e09bfcac97154f04f0634687
ayouboublouch <Oublouchayoub123@gmail.com>
2024-07-08 17:57:04 +0100

    vercel commit3

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'esources/css/app.css',
-                'esources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    build: {
-        outDir: 'dist', // Add this line to specify the output directory
-    },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist', // Output directory
+  },
 });
